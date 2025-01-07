f9cb689730220ffcab8c3bd7fd2fe7b9257d666d
OnyxAI-Dev <christianpham1503@gmail.com>
2025-01-07 19:25:50 +0700

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,16 @@
+# Application Metadata
+APP_NAME = "OnyxAI"
+VERSION = "1.0.0"
+DEBUG = True
+
+# Blockchain Settings
+BLOCKCHAIN_PROVIDER = "https://example-blockchain-node.com"
+DEFAULT_GAS_LIMIT = 30000
+
+# AI Settings
+AI_MODEL_PATH = "./models/onyx_model"
+AI_LOG_LEVEL = "INFO"
+
+# Logging Settings
+LOG_FILE = "onyxai.log"
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
